fa0944e3e949da0e8c13267510af9331af192803
Carlos Alberto Chavarria Ortiz <carlosalberto.chavarriao@gmail.com>
2021-10-27 00:47:15 -0500

    subiendo archivos

---
+++ biblioteca.sql
@@ -0,0 +1,10 @@
+#INSERT INTO rentas VALUES(NULL, 3, 2, "2021-10-20", 28/10/2021, 1);
+/*UPDATE libros SET fecha_publicacion = "2021-05-20" WHERE id_libros = 1;
+UPDATE libros SET fecha_publicacion = "2021-10-12" WHERE id_libros = 2;
+UPDATE libros SET fecha_publicacion = "2021-05-20" WHERE id_libros = 3;
+UPDATE libros SET fecha_publicacion = "1989-05-13" WHERE id_libros = 4;
+UPDATE libros SET fecha_publicacion = "1999-02-20" WHERE id_libros = 5;*/
+DELETE FROM rentas  WHERE id_rentas = 1;
+INSERT INTO rentas VALUES(1, 3, 3, "2021-10-26", "2021-10-26", 2);
+SELECT * FROM rentas;
+SELECT * FROM rentas;
